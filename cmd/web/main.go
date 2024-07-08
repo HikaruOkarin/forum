96bc50f7e997e12e0cd79dd317d33737df9f5d28
dkurmant <didarak179@gmail.com>
2024-07-09 02:11:51 +0500

    five

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network adress")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", Home)
 	mux.HandleFunc("/snippet", ShowSnippet)
 	mux.HandleFunc("/snippet/create", CreateSnippet)
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
-	log.Println("starting server in http://localhost:4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Println("Starting server on http://localhost" + *addr)
+
+	err := http.ListenAndServe(*addr, mux)
 	if err != nil {
 		log.Fatal(err)
 	}
