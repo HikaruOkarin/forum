44bbe61e8c4fa4d7a20772842efffccee175aaf9
HikaruOkarin <didarak179@gmail.com>
2024-07-13 14:58:33 +0600

    add helpers.go

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
+func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -21,7 +20,8 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 	tmpl, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Println(err.Error())
+		app.errorLog.Println(err.Error())
+
 		http.Error(w, "Internal Server Error", 500)
 		return
 
@@ -29,41 +29,27 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 	err = tmpl.Execute(w, nil)
 	if err != nil {
-		log.Println(err.Error())
+		app.errorLog.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 		return
 	}
 }
 
-func ShowSnippet(w http.ResponseWriter, r *http.Request) {
-
+func (app *application) ShowSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-
 	if err != nil || id < 1 {
 		http.Error(w, "404 page not found", 404)
 		return
 	}
-
-	info := fmt.Sprintf("number of snipet id = %d", id)
+	info := fmt.Sprintf("number of snippet id = %d", id)
 	w.Write([]byte(info + "\n"))
 }
 
-func CreateSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) CreateSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-
-		w.Header()["anime-type"] = []string{"1;mode=block"}
-		w.Header().Set("cache-control", "naruto uzumaki")
-		w.Header().Add("Cache-Control", "public")
-		w.Header().Add("Cache-Control", "max-age=31536000")
-		w.Header()["Date"] = nil //for delete system generated headers
-
-		data := w.Header().Get("Cache-Control")
-		fmt.Println(data)
-
-		w.Header().Del("Allow")                  // for delete header
-		http.Error(w, "Method Not Allowed", 405) // w.WriteHeader(405) => использовать тока один раз && w.Write([]byte("some text"))
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Method Not Allowed", 405)
 		return
 	}
-
-	fmt.Fprintln(w, "Create a new snippet")
+	w.Write([]byte("Create a new post"))
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,22 +4,38 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network adress")
 	flag.Parse()
 
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", Home)
-	mux.HandleFunc("/snippet", ShowSnippet)
-	mux.HandleFunc("/snippet/create", CreateSnippet)
+	mux.HandleFunc("/", app.Home)
+	mux.HandleFunc("/snippet", app.ShowSnippet)
+	mux.HandleFunc("/snippet/create", app.CreateSnippet)
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
-	log.Println("Starting server on http://localhost" + *addr)
-
-	err := http.ListenAndServe(*addr, mux)
-	if err != nil {
-		log.Fatal(err)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
 	}
+
+	infoLog.Printf("Starting server on http://localhost%s", *addr)
+
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
