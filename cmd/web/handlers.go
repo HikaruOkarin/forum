dad4ffaa6c4688f8698a06888f300664b0c959d2
HikaruOkarin <kurmantaevdidar77@gmail.com>
2024-08-20 19:49:58 +0500

    add parser

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,10 +9,7 @@ import (
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
+
 	s, err := app.posts.Latest()
 	if err != nil {
 		app.serverError(w, err)
@@ -20,12 +17,13 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 	}
 	data := &templateData{Posts: s}
 
-	app.render(w, r, "home.page.tmpl",data)
+	app.render(w, r, "home.page.tmpl", data)
 
 }
 
 func (app *application) ShowSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -44,21 +42,30 @@ func (app *application) ShowSnippet(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (app *application) CreateSnippet(w http.ResponseWriter, r *http.Request) {
-	// if r.Method != http.MethodPost {
-	// 	w.Header().Set("Allow", "POST")
-	// 	app.clientError(w, http.StatusMethodNotAllowed)
-	// 	return
-	// }
+func (app *application) CreateSnippetForm(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "create.page.tmpl", nil)
 
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n Kobayashi"
-	expires := "7"
+}
+
+func (app *application) CreateSnippet(w http.ResponseWriter, r *http.Request) {
+
+	err := r.ParseForm()
+	if err != nil {
+
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+	expires := r.PostForm.Get("expires")
 	id, err := app.posts.Insert(title, content, expires)
 	if err != nil {
+
 		app.serverError(w, err)
 		return
 	}
 
-	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
+
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,14 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/bmizerany/pat"
+	"github.com/justinas/alice"
+)
 
 func (app *application) routes() http.Handler {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.Home)
-	mux.HandleFunc("/snippet", app.ShowSnippet)
-	mux.HandleFunc("/snippet/create", app.CreateSnippet)
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
 
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+	mux := pat.New()
+	mux.Get("/", http.HandlerFunc(app.Home))
+	mux.Get("/snippet/create", http.HandlerFunc(app.CreateSnippetForm))
+	mux.Post("/snippet/create", http.HandlerFunc(app.CreateSnippet))
+	mux.Get("/snippet/:id", http.HandlerFunc(app.ShowSnippet))
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Get("/static/", http.StripPrefix("/static/", fileServer))
+
+	return standardMiddleware.Then(mux)
 }
